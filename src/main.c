0b5d43a40ee9c175b4574d10cc9c277b679d09ef
Aldana Pocai <38792460+aldanapocai@users.noreply.github.com>
2023-12-23 20:45:03 -0300

    Registers set by Memory Address

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
-#include <avr/io.h>
+//#include <avr/io.h>
+#include <stdint.h>
+#define DDB5 5
+#define PORTB5 5
+volatile uint8_t* PORTB_ADDRESS = (volatile uint8_t*) 0x25;
+volatile uint8_t* DDRB_ADDRESS = (volatile uint8_t*) 0x24;
 
-#define F_CPU 16000000UL   //Arduino UNO - Clock Frequency 16MHz
+//#define F_CPU 16000000UL   //Arduino UNO - Clock Frequency 16MHz
 
 
 void delay_ms_custom(uint16_t ms) {
@@ -11,12 +16,15 @@ void delay_ms_custom(uint16_t ms) {
 }
 
 int main(void){
-  DDRB = DDRB | (1 << DDB5);
+  //DDRB = DDRB | (1 << DDB5);
+  *DDRB_ADDRESS = *DDRB_ADDRESS | (1 << DDB5);
 
   while(1) {
-  PORTB = PORTB | (1 << PORTB5);
+  *PORTB_ADDRESS = *PORTB_ADDRESS | (1 << PORTB5);  
+  //PORTB = PORTB | (1 << PORTB5);
   delay_ms_custom(1000);
-  PORTB = PORTB & ~(1 << PORTB5);
+  *PORTB_ADDRESS = *PORTB_ADDRESS & ~(1 << PORTB5);
+  //PORTB = PORTB & ~(1 << PORTB5);
   delay_ms_custom(1000);
 }
   return 0;
