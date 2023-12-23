fe721bcdbcc97309d4f1ce992f024dde07c8783a
Aldana Pocai <38792460+aldanapocai@users.noreply.github.com>
2023-12-23 20:55:38 -0300

    Refactored code

--- src/main.c
+++ src/main.c
@@ -1,31 +1,31 @@
-//#include <avr/io.h>
 #include <stdint.h>
+
 #define DDB5 5
 #define PORTB5 5
-volatile uint8_t* PORTB_ADDRESS = (volatile uint8_t*) 0x25;
-volatile uint8_t* DDRB_ADDRESS = (volatile uint8_t*) 0x24;
+#define PORTB_ADDRESS *((volatile uint8_t*) 0x25)
+#define DDRB_ADDRESS *((volatile uint8_t*) 0x24)
 
-//#define F_CPU 16000000UL   //Arduino UNO - Clock Frequency 16MHz
-
+//volatile uint8_t* PORTB_ADDRESS = (volatile uint8_t*) 0x25;
+//volatile uint8_t* DDRB_ADDRESS = (volatile uint8_t*) 0x24;
 
 void delay_ms_custom(uint16_t ms) {
-    uint16_t i, j;
-    for (i = 0; i < ms; i++)
-        for (j = 0; j < 3200; j++) //Ajustado 16000/4
-            asm volatile("nop"); // Delay for 1ms
+  uint16_t i, j;
+  for (i = 0; i < ms; i++) {
+    for (j = 0; j < 3200; j++) { // Ajustado 16000/4
+      asm volatile("nop"); // Delay for 1ms
+    }
+  }
 }
 
-int main(void){
-  //DDRB = DDRB | (1 << DDB5);
-  *DDRB_ADDRESS = *DDRB_ADDRESS | (1 << DDB5);
+int main(void) {
+  DDRB_ADDRESS = DDRB_ADDRESS | (1 << DDB5);
 
-  while(1) {
-  *PORTB_ADDRESS = *PORTB_ADDRESS | (1 << PORTB5);  
-  //PORTB = PORTB | (1 << PORTB5);
-  delay_ms_custom(1000);
-  *PORTB_ADDRESS = *PORTB_ADDRESS & ~(1 << PORTB5);
-  //PORTB = PORTB & ~(1 << PORTB5);
-  delay_ms_custom(1000);
-}
+  while (1) {
+    PORTB_ADDRESS = PORTB_ADDRESS | (1 << PORTB5);
+    delay_ms_custom(1000);
+    PORTB_ADDRESS = PORTB_ADDRESS & ~(1 << PORTB5);
+    delay_ms_custom(1000);
+  }
+
   return 0;
 }
